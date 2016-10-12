84ade91b66a546b467b3a834dc8f542159292873
Chris Kaschner <chris.kaschner@gmail.com>
2016-10-12 17:58:22 -0500

    ADD refactor to separate files

---
+++ handlers.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func ImagesIndex(w http.ResponseWriter, r *http.Request) {
+	images := Images{
+		Image{Title: "Nikes", Url: "http://imgdirect.s3-website-us-west-2.amazonaws.com/nike.jpg"},
+		Image{Title: "Altras", Url: "https://s3-us-west-2.amazonaws.com/imgdirect/altra.jpg"},
+	}
+
+	if err := json.NewEncoder(w).Encode(images); err != nil {
+		panic(err)
+	}
+}
+
+func GetImage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["ImgId"]
+	fmt.Fprintln(w, "Get Image:", todoId)
+}
